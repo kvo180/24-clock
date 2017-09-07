1be31579bf59842f6f2c789bfa93fe010a88fe0f
kvo180 <khoavo180@gmail.com>
2017-09-06 19:56:56 -0700

    Move index.js out from server dir

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let express = require('express');
 let app = express();
 let path = require('path');
 
-app.use(express.static(path.join(__dirname,  '../client/dist')));
+app.use(express.static(path.join(__dirname,  '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
 });
 
 app.listen(1337, () => {
